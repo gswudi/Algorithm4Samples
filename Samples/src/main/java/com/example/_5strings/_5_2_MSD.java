e7021072ea4b9479247b40e4415241be7f6fca1e
gaosheng <gaosheng@gmail.com>
2016-03-17 12:36:39 +0800

    MSD with insertion

--- Samples/src/main/java/com/example/_5strings/_5_2_MSD.java
+++ Samples/src/main/java/com/example/_5strings/_5_2_MSD.java
@@ -1,14 +1,26 @@
 package com.example._5strings;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * most-signification-digit first string sort
  * Created by gaosheng on 16-3-15.
  */
 public class _5_2_MSD {
 
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        String[] a = in.readAllStrings();
+        sort(a);
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+
     private static final int R = 256;   //radix
     private static final int M = 15;    //小数组的切换阈值
-    private static String[] aux;        //数据分类的辅助数组
 
     /**
      * @return -1 if d >= s.length()
@@ -19,12 +31,16 @@ public class _5_2_MSD {
     }
 
     public static void sort(String[] a) {
-        aux = new String[a.length];
-        sort(a, 0, a.length - 1, 0);
+        String[] aux = new String[a.length];
+        int N = a.length - 1;
+        sort(a, 0, N, 0, aux);
     }
 
-    private static void sort(String[] a, int lo, int hi, int d) {
-        if (hi < lo) return;
+    private static void sort(String[] a, int lo, int hi, int d, String[] aux) {
+        if (hi < lo + M) {
+            insertion(a, lo, hi, d);
+            return;
+        }
         int[] count = new int[R + 2];   //count[0] = 0, count[1] is index of "",count[r+2] is index of r
         for (int i = lo; i <= hi; i++) {
             int index = charAt(a[i], d) + 2;
@@ -42,8 +58,22 @@ public class _5_2_MSD {
             a[i] = aux[i - lo];
         }
         for (int r = 0; r < R; r++) {
-            sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1);
+            sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1, aux);
         }
+    }
+
+    private static void insertion(String[] a, int lo, int hi, int d) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j - 1], d); j--) {
+                exchange(a, j, j - 1);
+            }
+        }
+    }
+
+    private static void exchange(String[] a, int i, int j) {
+        String temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 
     private static boolean less(String v, String w, int d) {
